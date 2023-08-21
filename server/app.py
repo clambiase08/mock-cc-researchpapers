712a631556d4fb2634422352b1a69c54c3e65e69
Christina Lambiase <christina.lambiase@gmail.com>
2023-08-20 18:26:45 -0700

    complete

--- server/app.py
+++ server/app.py
@@ -2,27 +2,76 @@
 
 from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
-from flask_restful import Api
+from flask_restful import Api, Resource, abort
 
-from models import db, Research, Author, ResearchAuthors
+from models import db, Research, Author, ResearchAuthor
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def index():
-    return '<h1>Code challenge</h1>'
+api = Api(app)
 
-@app.route('/research')
-def restaurants():
 
-    pass
+class ResearchResource(Resource):
+    def get(self):
+        research = [
+            r.to_dict(rules=("-researchauthors",)) for r in Research.query.all()
+        ]
+        return make_response(research, 200)
 
-if __name__ == '__main__':
+
+api.add_resource(ResearchResource, "/research")
+
+
+class ResearchById(Resource):
+    def get(self, id):
+        research = Research.query.get_or_404(
+            id, description="Research paper not found"
+        ).to_dict(rules=("-researchauthors",))
+        return make_response(research, 200)
+
+    def delete(self, id):
+        research = Research.query.get_or_404(id, description="Research paper not found")
+        db.session.delete(research)
+        db.session.commit()
+        return make_response("", 204)
+
+
+api.add_resource(ResearchById, "/research/<int:id>")
+
+
+class Authors(Resource):
+    def get(self):
+        authors = [a.to_dict(rules=("-researchauthors",)) for a in Author.query.all()]
+        return make_response(authors, 200)
+
+
+api.add_resource(Authors, "/authors")
+
+
+class ResearchAuthors(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            new_researchauthors = ResearchAuthor(**data)
+        except Exception as e:
+            abort(406, errors=["validation errors"])
+        db.session.add(new_researchauthors)
+        db.session.commit()
+        return make_response(
+            new_researchauthors.to_dict(),
+            201,
+        )
+
+
+api.add_resource(ResearchAuthors, "/research_author")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -4,10 +4,81 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
+
+
+class Research(db.Model, SerializerMixin):
+    __tablename__ = "research"
+    serialize_rules = (
+        "-researchauthors.research",
+        "-created_at",
+        "-updated_at",
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String)
+    year = db.Column(db.Integer, nullable=False)
+    page_count = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    researchauthors = db.relationship(
+        "ResearchAuthor", backref="research", cascade="delete"
+    )
+
+    @validates("year")
+    def validate_year(self, key, year):
+        if not len(year) == 4:
+            raise ValueError("Year must be 4 digits")
+        return year
+
+
+class ResearchAuthor(db.Model, SerializerMixin):
+    __tablename__ = "researchauthors"
+    serialize_rules = (
+        "-author.researchauthors",
+        "-research.researchauthors",
+        "-created_at",
+        "-updated_at",
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey("authors.id"))
+    research_id = db.Column(db.Integer, db.ForeignKey("research.id"))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = "authors"
+    serialize_rules = (
+        "-researchauthors.authors",
+        "-created_at",
+        "-updated_at",
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    field_of_study = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    researchauthors = db.relationship(
+        "ResearchAuthor", backref="author", cascade="delete"
+    )
+
+    @validates("field_of_study")
+    def validate_field(self, key, field):
+        study = ["AI", "Robotics", "Machine Learning", "Vision", "Cybersecurity"]
+        if field not in study:
+            raise ValueError("Invalid field")
+        return field
+
 
 # Add models here
